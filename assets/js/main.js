1e0bdf80dab633c17394ade97815d5ca42adeed8
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-09-03 22:09:17 +0900

    webpack configuration 에서 input/output 경로 설정 필요

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,6 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/client/js/main.js":
@@ -16,7 +15,18 @@
   \*******************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/styles.scss */ \"./src/client/scss/styles.scss\");\n\nconsole.log(\"hi\");\n\n//# sourceURL=webpack://study-app/./src/client/js/main.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/styles.scss */ \"./src/client/scss/styles.scss\");\n/* harmony import */ var _nav__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./nav */ \"./src/client/js/nav.js\");\n/* harmony import */ var _nav__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_nav__WEBPACK_IMPORTED_MODULE_1__);\n\n\n\n//# sourceURL=webpack://study-app/./src/client/js/main.js?");
+
+/***/ }),
+
+/***/ "./src/client/js/nav.js":
+/*!******************************!*\
+  !*** ./src/client/js/nav.js ***!
+  \******************************/
+/***/ (() => {
+
+eval("var closeBtn = document.getElementById(\"closeBtn\");\nvar openBtn = document.getElementById(\"openBtn\");\nvar sideBar = document.getElementById(\"sideBar\");\n\nvar handleClose = function handleClose() {\n  sideBar.classList.replace(\"opened\", \"closed\");\n};\n\nvar handleOpen = function handleOpen() {\n  sideBar.classList.replace(\"closed\", \"opened\");\n};\n\ncloseBtn.addEventListener(\"click\", handleClose);\nopenBtn.addEventListener(\"click\", handleOpen);\n\n//# sourceURL=webpack://study-app/./src/client/js/nav.js?");
 
 /***/ }),
 
@@ -26,6 +36,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scs
   \*************************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+"use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://study-app/./src/client/scss/styles.scss?");
 
 /***/ })
@@ -57,6 +68,35 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
 /******/ 	}
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
 /******/ 	(() => {
 /******/ 		// define __esModule on exports
---
+++ src/client/js/nav.js
@@ -0,0 +1,15 @@
+const closeBtn = document.getElementById("closeBtn");
+const openBtn = document.getElementById("openBtn");
+const sideBar = document.getElementById("sideBar");
+
+
+const handleClose = () => {
+    sideBar.classList.replace("opened", "closed");
+}
+
+const handleOpen = () => {
+    sideBar.classList.replace("closed", "opened");
+}
+
+closeBtn.addEventListener("click", handleClose);
+openBtn.addEventListener("click", handleOpen);
